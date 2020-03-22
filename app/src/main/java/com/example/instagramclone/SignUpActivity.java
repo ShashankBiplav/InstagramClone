d518300a7fdd5023f7a8f34312dbfbcd4027d84b
ShashankBiplav <Biplavshashank7@gmail.com>
2020-03-22 17:29:13 +0530

    Get data from parse Server

--- app/src/main/java/com/example/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.java
@@ -6,19 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonSave;
 
     private EditText edtName, edtPunchSpeed, edtPunchPower, edtKickSpeed, edtKickPower;
+
+    private TextView txtGetData;
+
+    private Button buttonGetAllData;
+
+    private String allKickBoxers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,57 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         edtPunchPower = findViewById(R.id.edtPunchPower);
         edtKickSpeed = findViewById(R.id.edtKickSpeed);
         edtKickPower = findViewById(R.id.edtKickPower);
+        txtGetData = findViewById(R.id.txtGetData);
+        buttonGetAllData = findViewById(R.id.buttonGetAllData);
 
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery =ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("t6DsywrEWl", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if (object !=null && e==null){
+                            txtGetData.setText(object.get("name")+"-" +" Punch Power" + object.get("punchPower"));
+                        }
+                    }
+                });
+            }
+        });
+        buttonGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allKickBoxers="";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e==null){
+                            if (objects.size() > 0){
+
+                                for (ParseObject kickBoxer : objects) {
+                                    allKickBoxers = allKickBoxers + kickBoxer.get("name") + "\n";
+                                }
+
+                                FancyToast.makeText(SignUpActivity.this,
+                                        allKickBoxers,
+                                        FancyToast.LENGTH_LONG,
+                                        FancyToast.SUCCESS,
+                                        true).show();
+                            }
+                            else {
+                                FancyToast.makeText(SignUpActivity.this,
+                                        e.getMessage(),
+                                        FancyToast.LENGTH_LONG,
+                                        FancyToast.ERROR,
+                                        true).show();
+
+                            }
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -53,7 +115,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                 @Override
                 public void done(ParseException e) {
                     if (e == null) {
-                        FancyToast.makeText(SignUpActivity.this, kickBoxer.get("name") + " is saved to the Server !", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        FancyToast.makeText(SignUpActivity.this,
+                                kickBoxer.get("name") + " is saved to the Server !",
+                                FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
 
                     } else {
                         FancyToast.makeText(SignUpActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
@@ -62,7 +126,6 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             });
         }
         catch (Exception e){
-
             FancyToast.makeText(SignUpActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
         }
