3a4ef593e105c5753c2ce72fd8ac5b8081b7053b
ShashankBiplav <Biplavshashank7@gmail.com>
2020-03-27 16:11:10 +0530

    SignUp and Login Parse User

--- app/src/main/java/com/example/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -102,10 +103,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
 
-        buttonTransition.setOnClickListener(new View.OnClickListener() {
+        buttonTransition.setOnClickListener(new View.OnClickListener() { //switching activity
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(SignUpActivity.this, SignUpLoginActivity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
@@ -1,4 +1,81 @@
 package com.example.instagramclone;
 
-public class SignUpLoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsernameSignup, editTextPasswordSignup, editTextUsernameLogin, editTextPasswordLogin;
+
+    private Button buttonSignup, buttonLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) { // savedInstanceState holds the state of the application
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.signup_login_activity);
+
+        editTextUsernameSignup = findViewById(R.id.editTextUsernameSignup);
+        editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
+        editTextUsernameLogin = findViewById(R.id.editTextUsernameLogin);
+        editTextPasswordLogin = findViewById(R.id.editTextPasswordLogin);
+        buttonSignup = findViewById(R.id.buttonSignUp);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ParseUser appUser = new ParseUser();
+                appUser.setUsername(editTextUsernameSignup.getText().toString());
+                appUser.setPassword(editTextPasswordSignup.getText().toString());
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if ( e== null){
+                            FancyToast.makeText(SignUpLoginActivity.this,
+                                    appUser.get("username")+" is Signed Up Successfully!",
+                                    FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        }
+                        else{
+                            FancyToast.makeText(SignUpLoginActivity.this,
+                                    e.getMessage(),
+                                    FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logInInBackground(editTextUsernameLogin.getText().toString(),
+                        editTextPasswordLogin.getText().toString(), new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+                                if (user != null && e== null){
+                                    FancyToast.makeText(SignUpLoginActivity.this, user.get("username")+" is Logged in Successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                                }
+                                else {
+                                    FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
 }
